681ade7ec9afd2fbbec535d6afb95c37306f398e
kimonkekes <kimonkekes1@gmail.com>
2024-02-28 15:06:16 +0200

    Changed title bar appearance

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,43 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
-import { cats } from "./cats";
 import SearchBox from "./SearchBox";
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      cats: cats,
+      cats: [],
       searchfield: ''
     }
   }
 
-  onSearchChange = (event) => {
-    this.setState({searchfield: event.target.value}, 
-    () => console.log(this.state.searchfield))
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users => this.setState({ cats: users}))
   }
 
-
-
+  onSearchChange = (event) => {
+    this.setState({searchfield: event.target.value})
+  }
 
   render() {
     const filteredCats = this.state.cats.filter(cat =>{
       return cat.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
     })
-    return (
-      <>
-      <div className='tc'>
-        <h1 className="f-headline">FantastiCATS</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList cats={filteredCats} />
-      </div>
-      </>
-    )
+    if (this.state.cats.length === 0) {
+      return <h1>Gathering cats. Please wait</h1>
+    } else {
+      return (
+        <>
+        <div className='tc'>
+          <h1 className="f-headline">FantastiCATS</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <CardList cats={filteredCats} />
+        </div>
+        </>
+      )
+    }
   }
 }
 
